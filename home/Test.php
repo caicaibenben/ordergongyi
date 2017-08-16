d02a89218cca91a2bbc36955b692be4ea56ac923
caicaibenben <179040604@qq.com>
2017-08-16 18:06:34 +0800

    order

---
+++ home/Test.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en-zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>奥德公益</title>
+    <link type="image/x-icon" href="" rel="icon">
+    <link href="../image/ico.png" rel="shortcut icon">
+
+    <!-- 新 Bootstrap 核心 CSS 文件 -->
+    <link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <!-- 可选的Bootstrap主题文件（一般不使用） -->
+    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"></script>
+    <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+    <script src="https://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
+    <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="Test.css" type="text/css">
+</head>
+<body>
+<!--头部-->
+<div class="layout header">
+    <div id="logo" class="left" style="width: 400px">
+        <a><img src="../image/logo.png" alt="奥德公益"></a>
+    </div>
+    <div id="search" class="left" style="width: 250px">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="请输入你要搜索的内容">
+            <span class="input-group-btn" id="searchbtn">
+                <button id="searchbtn" class="btn btn-default" type="button">
+                    <span class="glyphicon glyphicon-search"> </span>
+                </button>
+            </span>
+        </div>
+    </div>
+    <div id="login_out" class="right" style="width: 300px;margin-left: 30px">
+        <img src="../image/gongyi.png" width="150px">
+        <button type="button" class="btn btn-default btn-sm btn_color">登录</button>
+        <button type="button" class="btn btn-default btn-sm btn_color">注册</button>
+    </div>
+</div>
+<!--标题栏-->
+<nav class="nav_color" role="navigation" >
+    <div class="layout">
+        <ul class="nav navbar-nav">
+            <li><a href="" target="_self">首页</a></li>
+            <li></li>
+            <li><a href="" target="_self">活动报道</a></li>
+            <li></li>
+            <li><a href="" target="_self">精准助学</a></li>
+            <li></li>
+            <li><a href="" target="_self">往期回顾</a></li>
+            <li></li>
+            <li><a href="" target="_self">展望未来</a></li>
+        </ul>
+    </div>
+</nav>
+</body>
+</html>
+
+
---
+++ home/index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en-zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>奥德公益</title>
+    <link type="image/x-icon" href="" rel="icon">
+    <link href="image/ico.png" rel="shortcut icon">
+    <link rel="stylesheet" href="index.css" type="text/css">
+    <!-- 新 Bootstrap 核心 CSS 文件 -->
+    <link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <!-- 可选的Bootstrap主题文件（一般不使用） -->
+    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"></script>
+    <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+    <script src="https://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
+    <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <!--头部-->
+    <div>
+        <div id="logo">
+
+        </div>
+        <div id="search">
+
+        </div>
+        <div id="login_out">
+
+        </div>
+    </div>
+    <!--标题栏-->
+    <nav>
+
+    </nav>
+    <div>
+        <!--活动报道-->
+        <section>
+
+        </section>
+        <!--精准助学-->
+        <section>
+
+        </section>
+        <!--往期回顾-->
+        <section>
+
+        </section>
+        <!--展望未来-->
+        <section>
+
+        </section>
+    </div>
+    <!--底部-->
+    <footer>
+
+    </footer>
+</body>
+</html>
+
+
