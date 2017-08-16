c2eaaa4b4ec4facc0ef7c513f3d95c26cff9b034
caicaibenben <179040604@qq.com>
2017-08-16 22:43:23 +0800

    picture

--- home/Test.php
+++ home/Test.php
@@ -20,43 +20,7 @@
     <link rel="stylesheet" href="Test.css" type="text/css">
 </head>
 <body>
-<!--头部-->
-<div class="layout header">
-    <div id="logo" class="left" style="width: 400px">
-        <a><img src="../image/logo.png" alt="奥德公益"></a>
-    </div>
-    <div id="search" class="left" style="width: 250px">
-        <div class="input-group">
-            <input type="text" class="form-control" placeholder="请输入你要搜索的内容">
-            <span class="input-group-btn" id="searchbtn">
-                <button id="searchbtn" class="btn btn-default" type="button">
-                    <span class="glyphicon glyphicon-search"> </span>
-                </button>
-            </span>
-        </div>
-    </div>
-    <div id="login_out" class="right" style="width: 300px;margin-left: 30px">
-        <img src="../image/gongyi.png" width="150px">
-        <button type="button" class="btn btn-default btn-sm btn_color">登录</button>
-        <button type="button" class="btn btn-default btn-sm btn_color">注册</button>
-    </div>
-</div>
-<!--标题栏-->
-<nav class="nav_color" role="navigation" >
-    <div class="layout">
-        <ul class="nav navbar-nav">
-            <li><a href="" target="_self">首页</a></li>
-            <li></li>
-            <li><a href="" target="_self">活动报道</a></li>
-            <li></li>
-            <li><a href="" target="_self">精准助学</a></li>
-            <li></li>
-            <li><a href="" target="_self">往期回顾</a></li>
-            <li></li>
-            <li><a href="" target="_self">展望未来</a></li>
-        </ul>
-    </div>
-</nav>
+
 </body>
 </html>
 
