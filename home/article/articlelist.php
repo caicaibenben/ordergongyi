ee0187aaf8df381ae5287f4df4ad7f949a894d30
caicaibenben <179040604@qq.com>
2017-09-13 22:07:57 +0800

    list

--- home/article/articlelist.php
+++ home/article/articlelist.php
@@ -2,8 +2,25 @@
 require_once "header.php";
 ?>
     <!--文章列表显示-->
-    <div class="article_list">
+<div class="article_list_box">
+    <div class="article_list_box_header">
+        活动报道
     </div>
+    <div class="article_show">
+        <div class="article_show_title">
+            12
+        </div>
+        <div class="article_show_text">
+            34
+        </div>
+        <div class="article_show_footer">
+            56
+        </div>
+    </div>
+    <div class="article_list_box_footer">
+
+    </div>
+</div>
 
 <?php
 require_once "./article_share.php";
